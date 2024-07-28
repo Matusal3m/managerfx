fa087cc407a29c2e56362d219ec99e155a3afc9a
Matusalém <matusalemdedeus@gmail.com>
2024-07-28 14:34:17 -0300

    opens to

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,12 @@
 module com.manager.managerfx {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
+    requires java.compiler;
 
-
+    opens com.manager.managerfx.model.entities to javafx.base;
     opens com.manager.managerfx to javafx.fxml;
+
     exports com.manager.managerfx;
     exports com.manager.managerfx.controllers;
     opens com.manager.managerfx.controllers to javafx.fxml;
