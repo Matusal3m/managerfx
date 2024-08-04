a0521e99ca2062db16986c07a1c26911758ab736
Matusalém <matusalemdedeus@gmail.com>
2024-08-04 16:01:30 -0300

    change some names

--- src/main/java/com/manager/managerfx/utils/NumberParser.java
+++ src/main/java/com/manager/managerfx/utils/NumberParser.java
@@ -1,7 +1,13 @@
 package com.manager.managerfx.utils;
 
+import com.manager.managerfx.model.entities.Product;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.List;
+
 public class NumberParser {
-    public static Double formatDecimal(CharSequence chars) {
+    public static Double parseDecimal(CharSequence chars) {
         double result = 0;
         int i = 0;
         int len = chars.length();
@@ -12,7 +18,7 @@ public class NumberParser {
 
         for (; i < len; i++) {
             char c = chars.charAt(i);
-            if (c == '.') {
+            if (c == '.' || c == ',') {
                 i++;
                 break;
             }
@@ -28,4 +34,15 @@ public class NumberParser {
 
         return result;
     }
+
+    public static List<Product> formatDoubleFromList(List<Product> products) {
+        for (Product product : products) {
+            double price = product.getPrice();
+            BigDecimal bd = new BigDecimal(price)
+                    .setScale(2, RoundingMode.HALF_DOWN);
+            product.setPrice(bd.doubleValue());
+        }
+        return products;
+    }
+
 }
